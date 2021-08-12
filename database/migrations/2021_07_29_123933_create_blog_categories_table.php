2123abd87bfa44966d5c4ef3cd67538fba13a2fe
dimaproz42 <42dimaproz42@gmail.com>
2021-08-12 18:02:23 +0300

    Fill database with lorem posts and categories using seeds and factories

--- database/migrations/2021_07_29_123933_create_blog_categories_table.php
+++ database/migrations/2021_07_29_123933_create_blog_categories_table.php
@@ -15,9 +15,9 @@ class CreateBlogCategoriesTable extends Migration
     {
         Schema::create('blog_categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('parent_id')->unsigned()->default(0);
+            $table->integer('parent_id')->unsigned()->default(1);
             $table->string('slug')->unique();
-            $table->string('titile');
+            $table->string('title');
             $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
