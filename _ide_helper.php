b0427f4261f59b3b27f891388cdc5108f0d5070b
DRAGONLORDX <42dimaproz42@gmail.com>
2021-08-23 18:17:13 +0300

    test

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.29 on 2021-07-29 13:02:47.
+ * Generated for Laravel 5.7.29 on 2021-08-23 14:55:00.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
